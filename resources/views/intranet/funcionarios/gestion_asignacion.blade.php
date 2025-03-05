8771695ff4ae28920b63ddde2060321366921d7a
cesarmaya1006 <cesarmaya1006@gmail.com>
2025-03-05 11:57:09 -0500

    Cm 05-03-2025-10

--- resources/views/intranet/funcionarios/gestion_asignacion.blade.php
+++ resources/views/intranet/funcionarios/gestion_asignacion.blade.php
@@ -745,7 +745,7 @@
                                                                     <td>Respuesta apelación</td>
                                                                 @endif
                                                                 <td class="text-justify"><a
-                                                                        href="{{ asset('documentos/tareas/' . $anexo->url) }}"
+                                                                        href="{{ route('usuario_descarga_respuestaPQR', ['id' => $anexo->id]) }}"
                                                                         target="_blank" rel="noopener noreferrer"><i
                                                                             class="fa fa-download" aria-hidden="true"></i></a></td>
                                                             </tr>
